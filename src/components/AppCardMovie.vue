d26fc8055ad9584918c38c48a464f455c2f6c3c0
CraciDenisStefan <crdenis13@gmail.com>
2023-06-26 12:35:48 +0200

    Milestone 0

---
+++ src/components/AppCardMovie.vue
@@ -0,0 +1,18 @@
+<script>
+import {store} from '../store.js';
+export default {
+    data(){
+        return{
+            store, 
+        }
+    }, 
+}
+</script>
+<template >
+    <div>
+        card
+    </div>
+</template>
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,23 @@
+<script>
+import {store} from '../store.js';
+import AppCardMovie from './AppCardMovie.vue';
+
+export default {
+    components:{
+        AppCardMovie,
+    },
+    data(){
+        return{
+            store, 
+        }
+    },
+}
+</script>
+<template >
+    <div>
+        <AppCardMovie/>
+    </div>
+</template>
+<style lang="scss" scoped>
+    
+</style>
