9628b2a357ed4ba3f6c2173659a0dd1d83b88ec7
CraciDenisStefan <crdenis13@gmail.com>
2023-06-27 11:57:27 +0200

    [FEAT]Miletone 3 , ok .

--- src/components/AppCardMovie.vue
+++ src/components/AppCardMovie.vue
@@ -19,13 +19,9 @@ export default {
   methods:{
         star(){
             let vote = Math.round(this.oneMovie.vote_average / 2);
-            return vote
-            
+            return vote  
         },
-        emtyStar(){
-            let emty = 5 - this.star();
-            return emty
-        }
+       
     }
 }
 </script>
@@ -36,8 +32,7 @@ export default {
         {{ oneMovie.original_title || oneMovie.original_name }}
         <img :src="flags" alt="">
         <div>
-            <div v-for="(stars, index) in star()" :key="index" ><i class="fa-solid fa-star"></i></div>
-            <div v-for="(star, index) in emtyStar() " :key="index" ><i class="fa-regular fa-star"></i></div>
+            <div><i  v-for="(i, index) in 5" :key="index" :class="i <= star() ? 'fa-solid' : 'fa-regular'"   class=" fa-star"></i></div>
         </div>
 
     </div>
